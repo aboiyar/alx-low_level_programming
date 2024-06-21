e56b7341eee52a4a26c2a9931eeff38fb7efd0ba
aboiyar <moses.aboiyar@gmail.com>
2024-06-21 06:26:40 -0700

    variables if else while manipulations fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - will print the alphabet in both lowercase and uppercase, followed by a new line
+ * main - will print the alphabet in both lowercase and uppercase,
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
