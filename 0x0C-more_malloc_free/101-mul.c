7f3956e949f0753440a775fec89d364ca975b9f3
aboiyar <moses.aboiyar@gmail.com>
2024-07-18 15:11:57 -0700

    this multiplication has an error 2 steps but can't see the details yet

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,42 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+#include "main.h"
 
-// Function to check if a string represents a valid positive number
-int is_valid_number(const char *str) {
-    while (*str) {
-        if (!isdigit(*str)) {
-            return 0; // Invalid character found
-        }
-        str++;
-    }
-    return 1; // All characters are digits
+/**
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+unsigned long mul;
+int i, j;
+	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
+
+	}
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
 }
-
-int main(int argc, char *argv[]) {
-    // Check if the number of arguments is correct
-    if (argc != 3) {
-        fprintf(stderr, "Error\n");
-        exit(98);
-    }
-
-    // Validate num1 and num2
-    if (!is_valid_number(argv[1]) || !is_valid_number(argv[2])) {
-        fprintf(stderr, "Error\n");
-        exit(98);
-    }
-
-    // Convert arguments to integers
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
-
-    // Perform multiplication
-    int result = num1 * num2;
-
-    // Print the result
-    printf("%d\n", result);
-
-    return 0;
-}
-
